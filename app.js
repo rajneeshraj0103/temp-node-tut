ca05ae2db0e0827256c34a7c1a68a47ece47fcc2
Rajneesh Raj <rajneesh.raj.0103@gmail.com>
2026-05-06 18:24:34 +0530

    sixth commit

--- app.js
+++ app.js
@@ -13,12 +13,19 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
-const http = require('http')
+var http = require('http')
+var fs = require('fs')
 
-const server = http.createServer()
-
-server.on('request', (req, res) => {
-    res.end('welcome')
-})
-
-server.listen(5000)
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
